437613da1e956a065255edba01c09ede952d200b
ian <ianschoenrock@gmail.com>
2023-02-11 16:44:04 -0700

    Added error handling

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
@@ -16,6 +16,9 @@ def post_detail(request, year, month, day, post):
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1) 
-    posts = paginator.page(page_number)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, 'blog/post/list.html', {'posts':posts})     
